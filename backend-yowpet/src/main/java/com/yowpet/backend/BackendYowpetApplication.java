0551099b69c3f7ca27b467fbec341a339f0cb843
gusgonza <yowcloud@outlook.es>
2024-11-19 20:39:05 +0100

    🎨feat: reestructuración del Dockerfile y configuración de Docker Compose 🎨

--- backend-yowpet/src/main/java/com/yowpet/backend/BackendYowpetApplication.java
+++ backend-yowpet/src/main/java/com/yowpet/backend/BackendYowpetApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BackendYowpetApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendYowpetApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BackendYowpetApplication.class, args);
+    }
 
 }
