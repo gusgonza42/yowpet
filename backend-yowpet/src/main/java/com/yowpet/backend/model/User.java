1df7e9d6ebbdde9678c8aee6528863623a0b9fc0
chechuldenus <chechuldenus@alumnes.ilerna.com>
2025-01-14 19:52:02 +0100

    🚀feat: tabla place_reviews creada

--- backend-yowpet/src/main/java/com/yowpet/backend/model/User.java
+++ backend-yowpet/src/main/java/com/yowpet/backend/model/User.java
@@ -1,4 +1,67 @@
 package com.yowpet.backend.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "user")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(length = 100, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Place_reviews> reviews;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Place_reviews> getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(List<Place_reviews> reviews) {
+        this.reviews = reviews;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
