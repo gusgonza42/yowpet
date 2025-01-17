f8a74c4ed2114ee3b77e4343e80791cdd174514f
ManoGirgis <119486867+ManoGirgis@users.noreply.github.com>
2025-01-17 18:19:46 +0100

    🛠️ fix: BBDD (Allergy-pet)

--- backend-yowpet/src/main/java/com/yowpet/backend/model/Allergy.java
+++ backend-yowpet/src/main/java/com/yowpet/backend/model/Allergy.java
@@ -2,42 +2,33 @@ package com.yowpet.backend.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.List;
 
 /**
  * Entity model to represent an Allergy.
- * Uses JPA annotations to map the class to a database table.
- *
- * Includes Lombok annotations to automatically generate getter, setter, constructors, and other methods.
  */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table ( name = "allergies" )
+@Table(name = "allergies")
 public class Allergy {
 
-    /**
-     * Unique identifier for the Allergy.
-     */
-
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private int al_id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "al_id")
+    private Long al_id;
 
-    /**
-     * Name of the animal.
-     */
+    @Column(name = "al_name", nullable = false)
     private String al_name;
 
-    private byte al_photo;
+    @Lob
+    @Column(name = "al_photo")
+    private byte[] al_photo;
 
     @ManyToMany(mappedBy = "allergies", fetch = FetchType.LAZY)
     @JsonIgnore
-    private List<Pet> p_id;
-
+    private List<Pet> pets;
 }
