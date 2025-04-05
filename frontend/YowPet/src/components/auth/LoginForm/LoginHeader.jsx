08a109212b3f8fb85c695afc5e8dc1bb6302714d
gusgonza <yowcloud@outlook.es>
2025-04-06 00:18:45 +0200

    ✨feat(app): Actualizar iconos de aplicaciones y pantalla de salpicaduras; Modificar el comando de paquete.json limpio

--- frontend/YowPet/src/components/auth/LoginForm/LoginHeader.jsx
+++ frontend/YowPet/src/components/auth/LoginForm/LoginHeader.jsx
@@ -1,11 +1,14 @@
-import { View, Image } from 'react-native';
+import { Image, View } from 'react-native';
 import { Text } from 'react-native-paper';
 import { styles } from '@components/auth/LoginForm/styles';
 
 export function LoginHeader() {
   return (
     <View style={styles.headerContainer}>
-      <Image source={require('@assets/icon.png')} style={styles.logo} />
+      <Image
+        source={require('@assets/logos/yowpet_icon_v2.png')}
+        style={styles.logo}
+      />
       <Text style={styles.title}>Bienvenido a YowPet</Text>
     </View>
   );
