32d9168709c3869e4d9b770d783a38da2f335ae3
Manuel <manuelgirgis0@gmail.com>
2025-04-10 17:01:53 +0200

    🛠️Fix: microservicio repo

--- security/jwt-security/src/main/java/com/auth/security/repository/UserReposJWT.java
+++ security/jwt-security/src/main/java/com/auth/security/repository/UserReposJWT.java
@@ -15,19 +15,28 @@ public class UserReposJWT {
 
     public UserJWT getUserByEmail(String email) {
         String sql = "CALL GetUserByEmail(?)";
-        return template.queryForObject(sql, new BeanPropertyRowMapper<>(UserJWT.class), email);
-
+        UserJWT userJWT = template.queryForObject(sql, new BeanPropertyRowMapper<>(UserJWT.class), email);
+        if (userJWT != null) {
+            return userJWT;
+        } else {
+            return null;
+        }
     }
 
     public UserJWT getUserByUsername(String username) {
         String sql = "CALL GetUserByUsername(?)";
-        return template.queryForObject(sql, new BeanPropertyRowMapper<>(UserJWT.class), username);
-
+        UserJWT userJWT = template.queryForObject(sql, new BeanPropertyRowMapper<>(UserJWT.class), username);
+        if (userJWT != null) {
+            return userJWT;
+        } else {
+            return null;
+        }
     }
 
     public void createUser(String firstName, String lastName, String username, String email, String password, String city, String address, String phoneNumber, int zipCode, String gender, String profilePicture, int role, String languages, String paymentMethod, java.util.Date birthDate, String token) {
         String sql = "CALL createUserandToken(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,?,?)";
-        template.update(sql, firstName, lastName, username, email, password, city, address, phoneNumber, zipCode, gender, profilePicture, role, languages, paymentMethod, new java.sql.Date(birthDate.getTime()), token);
+       int rowsAffected =template.update(sql, firstName, lastName, username, email, password, city, address, phoneNumber, zipCode, gender, profilePicture, role, languages, paymentMethod, new java.sql.Date(birthDate.getTime()), token);
+   System.out.println("Rows affected: " + rowsAffected);
     }
 
 }
